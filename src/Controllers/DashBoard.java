5fdc046e579ae4fe98a99cddee3c5e6f8092075b
Kavindu07873 <Dilshankavindu07715@gmail.com>
2022-07-12 10:01:17 +0530

    make interface

--- src/Controllers/DashBoard.java
+++ src/Controllers/DashBoard.java
@@ -1,4 +1,18 @@
 package Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class DashBoard {
+    public Button btnStudent;
+    public AnchorPane root;
+
+    public void btnOnActionStudent(ActionEvent actionEvent) throws IOException {
+        this.root.getChildren().clear();
+        this.root.getChildren().add(FXMLLoader.load(this.getClass().getResource("../view/Student.fxml")));
+    }
 }
